7f7305695dc6908e0397c8315c25e90abda879b3
38041 <alessarovigatti.talita@gmail.com>
2025-09-01 08:07:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+import TenisNike from './assets/tenis-nike.jpg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        <header></header>
+        <section className="produtos">
+
+          {/* Produto 1 */}
+          <a href="#" className="link-card">
+            <div className="card">
+              <img src={TenisNike} alt="" className="img-prod"/>
+              <h2>GLOSS FRAN BY FRANCINY EHLKE LIPHONEY</h2>
+              <p className="desc">Referência:   40103  Modelo/Marca:   Mboom</p>
+              <p className="preco">R$ 69,90</p>
+              <div className="avaliacao"> <span>★ ★ ★ ★ ☆</span>(472)</div>
+              <div className="off">- 15%</div>
+            </div>
+          </a>
+
+          {/* Produto 2 */}
+          <a href="#" className="link-card">
+            <div className="card">
+              <img src={TenisNike} alt="" className="img-prod"/>
+              <h2>KIT FRAN GLOSSLICIOUS BY FRANCINY EHLKE</h2>
+              <p className="desc">Referência:   12103 Modelo/Marca:   Mboom</p>
+              <p className="preco">R$ 109,90</p>
+              <div className="avaliacao">  <span>★ ★ ★ ☆ ☆</span>(128)</div>
+              <div className="off">- 15%</div>
+            </div>
+          </a>
+
+          {/* Produto 3 */}
+          <a href="#" className="link-card">
+            <div className="card">
+              <img src={TenisNike} alt="" className="img-prod"/>
+              <h2>GLOSS FRAN BY FRANCINY EHLKE LIP BUNNY</h2>
+              <p className="desc">Referência:   40104   Modelo/Marca:   Mboom</p>
+              <p className="preco">R$ 65,90</p>
+              <div className="avaliacao">  <span>★ ★ ★ ★ ★</span>(28)</div>
+              <div className="off">- 15%</div>
+            </div>
+          </a>
+
+        </section>
+        <section className="destaque"></section>
+        <footer></footer>
+      </main>
+    </>
+  )
+}
+
+export default App
